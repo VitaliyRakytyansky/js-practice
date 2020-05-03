83e151240c64a29e32c19187262e3aa4008633c0
vitalii.rakytianskyi <vitalii.rakytianskyi@avid.com>
2020-05-03 13:53:21 +0300

    Deleted old files.

--- lesson-01/first.js
+++
@@ -1,8 +0,0 @@
-let name = "Protective Field Generator";
-let price = 1000;
-
-console.log(`${name} was chosen, the price for one ${price}`);
-
-price = 2000;
-
-console.log(`${name} was chosen, the price for one ${price}`);
--- lesson-01/second.js
+++
@@ -1,8 +0,0 @@
-const total = 100;
-const ordered = 50;
-
-if (ordered > total) {
-  console.log("In stock not enough goods");
-} else {
-  console.log("The order is placed, the manager will contact you");
-}
