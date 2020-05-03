64e01eb99c88abca6ba70d9f8984e0a820d21186
vitalii.rakytianskyi <vitalii.rakytianskyi@avid.com>
2020-05-03 13:51:39 +0300

    Renamed files name.

---
+++ lesson-01/task-01.js
@@ -0,0 +1,8 @@
+let name = "Protective Field Generator";
+let price = 1000;
+
+console.log(`${name} was chosen, the price for one ${price}`);
+
+price = 2000;
+
+console.log(`${name} was chosen, the price for one ${price}`);
