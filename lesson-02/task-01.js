0324c552239d58ca6c36d3eeca2861c667dc5ee4
vitalii.rakytianskyi <vitalii.rakytianskyi@avid.com>
2020-05-04 11:14:56 +0300

    Deleted lesson`s folders.

--- lesson-02/task-01.js
+++
@@ -1,9 +0,0 @@
-let names = ["Mango", "Poly", "Ajax"];
-
-const logsItems = function (names) {
-  for (let i = 0; i < names.length; i++) {
-    console.log(`${i + 1} - ${names[i]}`);
-  }
-};
-
-logsItems(names);
--- lesson-02/task-05.js
+++
@@ -1,6 +0,0 @@
-const checkForSpan = function (sentence) {
-  sentence = sentence.toLowerCase();
-  return sentence.includes("sale") || sentence.includes("spam") ? true : false;
-};
-
-console.log(checkForSpan("[SPAM] How to earn fast money?"));
